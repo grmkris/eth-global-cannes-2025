de7a11e9b8b4ba2e4d8c3b377bc609d23bd54eb0
grmkris <kristjan.grm1@gmail.com>
2025-07-04 20:54:50 +0200

    qq

---
+++ apps/web/wagmi-config.ts
@@ -0,0 +1,27 @@
+
+// config/index.tsx
+
+import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
+import { mainnet, arbitrum } from '@reown/appkit/networks'
+import { cookieStorage, createStorage } from 'wagmi'
+
+// Get projectId from https://cloud.reown.com
+export const projectId = '252a2357b74167c2db0cfb4b03175d4f'
+
+if (!projectId) {
+  throw new Error('Project ID is not defined')
+}
+
+export const networks = [mainnet, arbitrum]
+
+//Set up the Wagmi Adapter (Config)
+export const wagmiAdapter = new WagmiAdapter({
+  storage: createStorage({
+    storage: cookieStorage
+  }),
+  ssr: true,
+  projectId,
+  networks
+})
+
+export const wagmiConfig = wagmiAdapter.wagmiConfig

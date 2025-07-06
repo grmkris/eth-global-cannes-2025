3d27c31687462db4f36d3ada7f9f3b38286c3ab5
LunarEchoesABC <snxjj25@gmail.com>
2025-07-06 07:24:22 +0200

    added avail nexus

--- apps/web/app/_lib/eoa.ts
+++ apps/web/app/_lib/eoa.ts
@@ -1,16 +1,22 @@
-import { createWalletClient, http, type Chain, type WalletClient } from "viem";
+import {
+  createWalletClient,
+  custom,
+  http,
+  type Chain,
+  type WalletClient,
+} from "viem";
 import { generatePrivateKey, privateKeyToAccount } from "viem/accounts";
 import { mainnet, sepolia, zircuitGarfieldTestnet } from "viem/chains";
 
 const CHAIN_ID_TO_CHAIN = {
   11155111: sepolia,
   48898: zircuitGarfieldTestnet,
-}
+};
 
-export const getOrCreateEoa = (chain: Chain) : WalletClient => {
+export const getOrCreateEoa = (chain: Chain): WalletClient => {
   // react from local storage or create new one
   const eoa = localStorage.getItem("eoa");
-  console.log('eoa', eoa)
+  console.log("eoa", eoa);
   if (eoa) {
     return createWalletClient({
       transport: http(),
@@ -24,20 +30,23 @@ export const getOrCreateEoa = (chain: Chain) : WalletClient => {
     account: privateKeyToAccount(privateKey as `0x${string}`),
     chain: chain,
   });
-  console.log('walletClient', walletClient)
+
+  console.log("walletClient", walletClient);
   localStorage.setItem("eoa", privateKey);
   return walletClient;
-}
+};
 
 export const clearEoa = () => {
   localStorage.removeItem("eoa");
-}
+};
 
 export const setActiveChain = (chain: Chain) => {
   localStorage.setItem("activeChain", chain.id.toString());
-}
+};
 
-export const getActiveChain = () : Chain => {
+export const getActiveChain = (): Chain => {
   const activeChain = localStorage.getItem("activeChain");
-  return activeChain ? CHAIN_ID_TO_CHAIN[parseInt(activeChain) as keyof typeof CHAIN_ID_TO_CHAIN] : sepolia;
-}
+  return activeChain
+    ? CHAIN_ID_TO_CHAIN[parseInt(activeChain) as keyof typeof CHAIN_ID_TO_CHAIN]
+    : sepolia;
+};

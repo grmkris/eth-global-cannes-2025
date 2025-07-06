4f89501b06d02f558968f144738f10c483384d10
grmkris <kristjan.grm1@gmail.com>
2025-07-06 05:39:53 +0200

    qq

--- apps/web/app/_lib/avail-comp.tsx
+++ apps/web/app/_lib/avail-comp.tsx
@@ -1,17 +1,18 @@
 import { useQuery } from "@tanstack/react-query"
 import { initializeAvail } from "./avail"
 import { useEoaWalletClient } from "./eoa-hooks"
-import type { RequestArguments } from "@avail-project/nexus"
+import type { EthereumProvider, RequestArguments } from "@avail-project/nexus"
 
 export const AvailComponent = () => {
 
   const wc = useEoaWalletClient()
+
+
   
 
   const avail = useQuery({
     queryKey: ['avail'],
-    queryFn: () => initializeAvail({
-    })
+    queryFn: () => initializeAvail()
   })
   return <div>AvailComponent</div>
 }
